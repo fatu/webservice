e177d80b548b7ca538523a77d1f3f6530e039bb2
fatu <fatu@ebay.com>
2015-09-22 16:42:52 +0800

    Updated spring-tutorial.

--- spring-tutorial/src/main/java/com/caveofprogramming/spring/test/Person.java
+++ spring-tutorial/src/main/java/com/caveofprogramming/spring/test/Person.java
@@ -1,7 +1,27 @@
 package com.caveofprogramming.spring.test;
 
 public class Person {
-	public void speak() {
+
+	private int id;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void speak() {
 		System.out.println("Hello! I'm a person.");
 	}
+
+    @Override
+    public String toString() {
+        return "Person{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            '}';
+    }
 }
